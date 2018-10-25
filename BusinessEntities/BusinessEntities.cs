ce3413ab097b2ce12ed2f7c91be0416b9c942284
FelipeMirande <felipemirande@gmail.com>
2018-10-25 03:23:54 -0300

    primera subideta

---
+++ BusinessEntities/BusinessEntities.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntities
+{
+    public class BusinessEntities
+    {
+        public BusinessEntities()
+        {
+            State = States.New;
+        }
+            
+          
+
+        private int _ID;
+        public int ID
+        {
+            set { _ID = value; }
+            get { return _ID; }
+        }
+
+        public enum States
+        {
+            New,
+            Modified,
+            Delete,
+            Unmodified
+        }
+
+        private States _State;
+        public States State
+        {
+            set { _State = value; }
+            get { return _State; }
+        }
+
+    }
+}
---
+++ BusinessEntities/Producto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntities
+{
+    public class Producto : BusinessEntities
+    {
+        private string _Descripcion;
+        public string Descripcion
+        {
+            set { _Descripcion = value; }
+            get { return _Descripcion; }
+        }
+
+        private float _Precio;
+        public float Precio
+        {
+            set { _Precio = value; }
+            get { return _Precio; }
+        }
+    }
+}
